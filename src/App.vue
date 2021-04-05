ccdda125cc8a59e760b38e427cfbd90a03d987df
marvin <moreno.marvin@yandex.ru>
2021-04-04 18:03:43 -0600

    Block and Result component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Reaction Timer </h1>
+
+  <button @click="start" :disabled="isPlaying" >Play</button>
+
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score"  />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results},
+  data(){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+
+},
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+
+
+    }
   }
 }
 </script>
@@ -20,7 +48,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <p> Reaction Time -> {{ score}} </p>
+  <p> This is the ranking of your click: {{rank}} </p>
+
+  
+
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 300){
+            this.rank = 'Fast fingers'
+        } else if (this.score < 500) {
+            this.rank = 'Rapid reflexes'
+        } else {
+            this.rank = 'Snail pace'
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
